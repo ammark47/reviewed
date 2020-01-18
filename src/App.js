c79f4e5baa284ed833121f337e21df81f90b0fb8
ammark47 <ammark47@yahoo.com>
2020-01-18 14:19:24 -0500

    quick start redux router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Contains 'Home', 'User' and 'NotFound'
+import * as components from './components/components';
+
+const App = ({ page }) => {
+  const Component = components[page]
+  return <Component />
 }
 
-export default App;
+const mapStateToProps = ({ page }) => ({ page })
+
+export default connect(mapStateToProps)(App)
